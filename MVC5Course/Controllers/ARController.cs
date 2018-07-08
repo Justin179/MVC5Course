c70b152d46630ff7a6ac737ccbdf9796d9868da3
Justin179 <justin.uob@gmail.com>
2018-07-08 17:01:36 +0800

    加入圖片的顯示與下載

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -24,6 +25,27 @@ namespace MVC5Course.Controllers
             string str = "Partial字串";
             // PartialView的話，沒有layout
             return PartialView("ViewTest", (object)str);
+        }
+
+        public ActionResult ContentTest()
+        {
+            return Content("Test Content!",
+                "text/plain",
+                Encoding.GetEncoding("Big5"));
+        }
+
+        public ActionResult FileTest(string dl)
+        {
+            if (String.IsNullOrEmpty(dl))
+            {
+                return File(Server.MapPath("~/App_Data/city_cafe.png"),
+                    "image/jpeg");
+            }
+            else
+            {
+                return File(Server.MapPath("~/App_Data/city_cafe.png"),
+                    "image/jpeg", "xxx_cafe.png");
+            }
         }
 
     }
