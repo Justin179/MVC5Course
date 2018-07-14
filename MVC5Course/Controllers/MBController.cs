87136409e57c581e3f65d6aaaee5312ad803288c
Justin179 <justin.uob@gmail.com>
2018-07-14 14:04:15 +0800

    alive

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : BaseController
+    {
+        public ActionResult Index()
+        {
+            var data = "Hello World";
+            ViewData.Model = data;
+            return View();
+        }
+
+        public ActionResult ViewBagDemo()
+        {
+            ViewBag.Text = "Hi";
+            return View();
+        }
+        public ActionResult ViewDataDemo()
+        {
+            ViewData["Text"] = "Hi";
+            return View();
+        }
+
+        public ActionResult TempDataSave()
+        {
+            TempData["Text"] = "Temp";
+
+            return RedirectToAction("TempDataDemo");
+        }
+
+        public ActionResult TempDataDemo()
+        {
+            return View();
+        }
+    }
+}
